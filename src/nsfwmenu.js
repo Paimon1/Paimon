d519e3ab81c1ec7148bc020b65aaa7c18532e113
Paimon1 <97750989+Paimon1@users.noreply.github.com>
2022-01-15 11:47:47 -0500

    ccc
    ccc

---
+++ src/nsfwmenu.js
@@ -0,0 +1,20 @@
+const nsfwmenu = (prefix, pushname) => {
+    return `*Comandos para ver pornito 🔞*
+*NSFW ✅*
+
+${prefix}culos
+${prefix}tetas
+${prefix}ahegao
+${prefix}pies
+${prefix}rule34
+${prefix}ecchi
+${prefix}neko
+${prefix}yaoi 
+
+"El yaoi es pa las chicas no solo los hombre se divierten"
+
+by shanduy`
+
+}
+
+exports.nsfwmenu = nsfwmenu
